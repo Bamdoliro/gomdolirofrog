a5b128b7a16e72d69ee33fcb73956303b124960d
seonghoon07 <kkukkuli07@gmail.com>
2023-06-13 01:14:23 +0900

    ADD :: 게임 기능 구현 - 게임 기능, 대체적인 베이스를 작성하였습니다. 본 커밋에는 코드의 가독성, 정렬은 신경쓰지 않고 기능만 구현하였습니다.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,56 @@
-// TODO-1: "게임 시작" 버튼을 누르면 단어 게임이 시작됩니다.
+import words from "../data/\bwords.js";
 
+// TODO-1: "게임 시작" 버튼을 누르면 단어 게임이 시작됩니다.
+const gameStart = document.querySelector('.gameStart');
+const gameOver = document.querySelector('.gameOver');
+const h2 = document.querySelector('.h2');
+const input = document.querySelector('.input');
+const submit = document.querySelector('.submit');
+const hidden = document.querySelector('.hidden');
+const score = document.querySelector('.score');
+let char = '';
+let scoreNumber = 0;
+
+const onGameStart = () => {
+  hidden.style.display = 'block';
+  gameOver.style.display = 'flex';
+  gameStart.style.display = 'none';
+  const randomValue = words[Math.floor(Math.random() * words.length)];
+  h2.innerText = randomValue;
+  char = randomValue;
+  score.innerText = 0;
+}
+const onGameOver = () => {
+  h2.innerText = '';
+  gameOver.style.display = 'none';
+  gameStart.style.display = 'flex';
+  hidden.style.display = 'none';
+  scoreNumber = 0;
+  score.innerText = '';
+  alert('게임이 종료되었습니다.');
+}
+const onSubmitClick = (event) => {
+  event.preventDefault();
+  if (input.value === char) {
+    console.log('정답');
+    input.value = '';
+    scoreNumber += 5;
+    score.innerText = scoreNumber;
+  }
+  else {
+    console.log('오류');
+    scoreNumber -= 3;
+    score.innerText = scoreNumber;
+    input.value = '';
+  }
+  const randomValue = words[Math.floor(Math.random() * words.length)];
+  h2.innerText = randomValue;
+  char = randomValue;
+}
+
+gameStart.addEventListener('click',onGameStart);
+gameOver.addEventListener('click',onGameOver);
+submit.addEventListener('click',onSubmitClick);
 // TODO-2: "data" 폴더의 "words.js" 파일을 import하여 단어들을 무작위로 표시합니다.
 
 // TODO-3: 사용자로부터 입력을 받기 위해 입력 필드를 사용합니다.
