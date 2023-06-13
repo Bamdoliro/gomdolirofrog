f2fd02c752bda3481ab7fd6636107e4c47847b0f
seonghoon07 <kkukkuli07@gmail.com>
2023-06-13 23:24:47 +0900

    REFACTOR :: 클래스명, 변수명 수정 - html의 무작위 단어 태그의 클래스를 h2에서 random-word로 바꾸었습니다. 또한 app.js의 h2 변수도 randomWord로 이름을 바꾸었습니다.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-import words from "../data/\bwords.js";
-
 // TODO-1: "게임 시작" 버튼을 누르면 단어 게임이 시작됩니다.
 
 // TODO-2: "data" 폴더의 "words.js" 파일을 import하여 단어들을 무작위로 표시합니다.
@@ -12,28 +10,32 @@ import words from "../data/\bwords.js";
 
 // TODO-6: 사용자가 "끝내기" 버튼을 클릭하면 점수를 초기화하고 "게임이 정상적으로 종료되었습니다"라는 메시지를 표시합니다.
 
+import words from "../data/\bwords.js";
+
 const gameStart = document.querySelector('.gameStart');
 const gameOver = document.querySelector('.gameOver');
-const h2 = document.querySelector('.h2');
+const randomWord = document.querySelector('.random-word');
 const input = document.querySelector('.input');
 const submit = document.querySelector('.submit');
 const form = document.querySelector('.form');
 const score = document.querySelector('.score');
-let char = '';
-let scoreNumber = 0;
+let char = ''; // 함수 안에서 지역변수로 랜덤값을 뽑아냈기 때문에 그 값을 전역변수에 저장해준다.
+let scoreNumber = 0; // 점수를 추가,제거 할 전역변수 추가
 
 const onGameStart = () => {
   const randomValue = words[Math.floor(Math.random() * words.length)];
+  // Math.random 메서드를 이용해 0~1 사이 랜덤 수 추출 후 배열 길이를 곱해준다.
+  // 그 값에 Math.floor 함수를 이용하여 내림을 해준다. 그러면 성공적으로 랜덤으로 배열 값을 뽑아낼 수 있다.
   form.style.display = 'flex';
   gameOver.style.display = 'flex';
   gameStart.style.display = 'none';
   input.focus();
-  h2.innerText = '제시된 단어 : ' + randomValue;
+  randomWord.innerText = '제시된 단어 : ' + randomValue;
   char = randomValue;
   score.innerText = '점수 : ' + 0;
 }
 const onGameOver = () => {
-  h2.innerText = '';
+  randomWord.innerText = '';
   gameOver.style.display = 'none';
   gameStart.style.display = 'flex';
   form.style.display = 'none';
@@ -42,7 +44,7 @@ const onGameOver = () => {
   alert('게임이 정상적으로 종료되었습니다.');
 }
 const onSubmitClick = (event) => {
-  event.preventDefault();
+  event.preventDefault(); // 기본 동작 방지
   if (input.value == '') {
     alert('값을 입력해야 합니다!');
   }
@@ -58,8 +60,8 @@ const onSubmitClick = (event) => {
     score.innerText = '점수 : ' + scoreNumber;
     input.value = '';
   }
-  const randomValue = words[Math.floor(Math.random() * words.length)];
-  h2.innerText = '제시된 단어 : ' + randomValue;
+  const randomValue = words[Math.floor(Math.random() * words.length)]; 
+  randomWord.innerText = '제시된 단어 : ' + randomValue;
   char = randomValue;
 }
 
