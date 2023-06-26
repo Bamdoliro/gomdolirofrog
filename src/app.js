faf3acc049c8e9b6d67912bd30266901bb1b040a
rkdalswl718 <rkdalswl070718@gmail.com>
2023-06-26 22:46:18 +0900

    DOCS :: 느낀점

--- src/app.js
+++ src/app.js
@@ -20,86 +20,86 @@ const resetBtn = document.querySelector("#reset");
 
 //시작버튼을 눌렀을 때
 const startGame = () => {
-  startGameBtn.style.display = "none"; // 게임 시작 버튼 숨기기
-  finishGameBtn.style.display = "block"; // 끝내기 버튼 보이기
-  input.style.display = "block"; // 입력 필드 보이기
-  check.style.display = "block"; // 확인 버튼 보이기
-  wrd.style.display = "block"; //단어 보이기
-  scr.style.display = "block"; //점수 보이기
-  hscr.style.display = "none"; //최고점 숨기기
-  resetBtn.style.display = "none"; //리셋버튼 숨기기
-  randomWord = getRandomWord(); // 랜덤 단어 불러오기
-  answer = randomWord.toLowerCase(); // 소문자 형태의 정답 저장
-  score = 0;
-  scoreDisplay.textContent = score;
-  wordDisplay.textContent = randomWord; // 랜덤 단어 표시
-  input.value = ""; // 입력 필드 초기화
-  input.focus(); //처음부터 입력할 수 있게
+    startGameBtn.style.display = "none"; // 게임 시작 버튼 숨기기
+    finishGameBtn.style.display = "block"; // 끝내기 버튼 보이기
+    input.style.display = "block"; // 입력 필드 보이기
+    check.style.display = "block"; // 확인 버튼 보이기
+    wrd.style.display = "block"; //단어 보이기
+    scr.style.display = "block"; //점수 보이기
+    hscr.style.display = "none"; //최고점 숨기기
+    resetBtn.style.display = "none"; //리셋버튼 숨기기
+    randomWord = getRandomWord(); // 랜덤 단어 불러오기
+    answer = randomWord.toLowerCase(); // 소문자 형태의 정답 저장
+    score = 0;
+    scoreDisplay.textContent = score;
+    wordDisplay.textContent = randomWord; // 랜덤 단어 표시
+    input.value = ""; // 입력 필드 초기화
+    input.focus(); //처음부터 입력할 수 있게
 };
 
 const getRandomWord = () => {
-  return words[Math.floor(Math.random() * words.length)];
-  // 무작위 단어 선택. 배열에서 단어 가져옴, floor로 반올림한 값의 배열 출력
-  // return 으로 배열에서 무작위로 단어를 선택하여 반환 (매번 다른 단어 호출)
+    return words[Math.floor(Math.random() * words.length)];
+    // 무작위 단어 선택. 배열에서 단어 가져옴, floor로 반올림한 값의 배열 출력
+    // return 으로 배열에서 무작위로 단어를 선택하여 반환 (매번 다른 단어 호출)
 };
 
 // 영어만 입력되게 하기
 const handleOnInput = () => {
-  const userInput = input.value;
-  const filteredInput = userInput.replace(/[^A-Za-z ]/g, "");
-  input.value = filteredInput;
+    const userInput = input.value;
+    const filteredInput = userInput.replace(/[^A-Za-z ]/g, "");
+    input.value = filteredInput;
 };
 
 //게임 끝내기 버튼을 눌렀을 때
 const finishGame = () => {
-  myScore = score;
-  recordHighScore(); //게임을 끝냈을 때 최고점수 갱신
-  score = 0;
-  scoreDisplay.textContent = score;
-  alert(`게임이 정상적으로 종료되었습니다. 점수는 ${myScore}점 입니다.`);
-  startGameBtn.style.display = "block"; // 게임 시작 버튼 보이기
-  finishGameBtn.style.display = "none"; // 끝내기 버튼 숨기기
-  input.style.display = "none"; // 입력 필드 숨기기
-  check.style.display = "none"; // 확인 버튼 숨기기
-  wrd.style.display = "none"; //단어 숨기기
-  scr.style.display = "none"; //점수 숨기기
-  hscr.style.display = "block"; //최고점 보이기
-  resetBtn.style.display = "block"; //리셋버튼 보이기
+    myScore = score;
+    recordHighScore(); //게임을 끝냈을 때 최고점수 갱신
+    score = 0;
+    scoreDisplay.textContent = score;
+    alert(`게임이 정상적으로 종료되었습니다. 점수는 ${myScore}점 입니다.`);
+    startGameBtn.style.display = "block"; // 게임 시작 버튼 보이기
+    finishGameBtn.style.display = "none"; // 끝내기 버튼 숨기기
+    input.style.display = "none"; // 입력 필드 숨기기
+    check.style.display = "none"; // 확인 버튼 숨기기
+    wrd.style.display = "none"; //단어 숨기기
+    scr.style.display = "none"; //점수 숨기기
+    hscr.style.display = "block"; //최고점 보이기
+    resetBtn.style.display = "block"; //리셋버튼 보이기
 };
 
 //확인버튼을 눌렀을 때
 const wordCheck = () => {
-  const userInput = input.value.toLowerCase(); // 입력값 소문자 변환
-  if (userInput === "") {
-    alert("정답을 입력하세요.");
-    return; //alert 실행 후 단어 변동 x
-  } else if (userInput === answer) {
-    score += 5; // 단어가 일치하면 +5점
-    scoreDisplay.textContent = score;
-  } else {
-    score -= 3; // 단어가 일치하지 않으면 -3점
-    scoreDisplay.textContent = score;
-  }
-  input.value = ""; // 입력 필드 초기화
-  randomWord = getRandomWord(); // 새로운 단어 선택
-  answer = randomWord.toLowerCase(); // 소문자 형태의 정답 저장
-  wordDisplay.textContent = randomWord; // 랜덤 단어 표시
+    const userInput = input.value.toLowerCase(); // 입력값 소문자 변환
+    if (userInput === "") {
+        alert("정답을 입력하세요.");
+        return; //alert 실행 후 단어 변동 x
+    } else if (userInput === answer) {
+        score += 5; // 단어가 일치하면 +5점
+        scoreDisplay.textContent = score;
+    } else {
+        score -= 3; // 단어가 일치하지 않으면 -3점
+        scoreDisplay.textContent = score;
+    }
+    input.value = ""; // 입력 필드 초기화
+    randomWord = getRandomWord(); // 새로운 단어 선택
+    answer = randomWord.toLowerCase(); // 소문자 형태의 정답 저장
+    wordDisplay.textContent = randomWord; // 랜덤 단어 표시
 };
 
 //최고점수 기록하기
 const recordHighScore = () => {
-  if (score > highScore) {
-    highScore = score;
-    highDisplay.textContent = highScore;
-    localStorage.setItem("highScore", highScore); // 최고 점수 로컬 스토리지에 저장하기
-  }
+    if (score > highScore) {
+        highScore = score;
+        highDisplay.textContent = highScore;
+        localStorage.setItem("highScore", highScore); // 최고 점수 로컬 스토리지에 저장하기
+    }
 };
 
 //최고점수 기록 리셋버튼을 눌렀을 때
 const reset = () => {
-  localStorage.clear(); // 로컬스토리지 초기화 시키기
-  highScore = 0; // 최고점수 초기화
-  highDisplay.textContent = highScore;
+    localStorage.clear(); // 로컬스토리지 초기화 시키기
+    highScore = 0; // 최고점수 초기화
+    highDisplay.textContent = highScore;
 };
 
 startGameBtn.addEventListener("click", startGame);
@@ -115,4 +115,4 @@ check.style.display = "none"; // 확인 버튼 숨기기
 finishGameBtn.style.display = "none"; // 끝내기 버튼 숨기기
 wrd.style.display = "none"; //단어 숨기기
 scr.style.display = "none"; //점수 숨기기
-highDisplay.textContent = highScore; // 로컬스토리지에 저장된 최고점 보이기'
+highDisplay.textContent = highScore; // 로컬스토리지에 저장된 최고점 보이기
